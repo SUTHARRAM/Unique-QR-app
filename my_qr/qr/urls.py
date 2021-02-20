84df29c232953f5f51d6f3f03dfbfb4a78fc8f26
SUTHARRAM <rls42847@gmail.com>
2021-02-20 12:29:52 +0530

    new-commit

--- my_qr/qr/urls.py
+++ my_qr/qr/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home),
-    path('display/', display),
+    path('display/', display, name='disp'),
     path('pdf/<int:pk>/', Pdf.as_view(), name = 'qrgen'),
 ]
